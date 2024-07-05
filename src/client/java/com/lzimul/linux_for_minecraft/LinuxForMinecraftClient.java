dfb27dff9b04a95b834a8725fb5fc534bf60b796
lZiMUl <lzimul@lzimul.top>
2024-07-04 23:54:14 -0700

    Init Push

---
+++ src/client/java/com/lzimul/linux_for_minecraft/LinuxForMinecraftClient.java
@@ -0,0 +1,10 @@
+package com.lzimul.linux_for_minecraft;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LinuxForMinecraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
